33830be3216223180411b1e51a421ed49b691ccb
Mamta Srinivas <mamtasrinivassp@gmail.com>
2020-02-05 22:42:39 +0100

    almost working

--- script.ts
+++ script.ts
@@ -3,14 +3,47 @@
 //     RotateClock
 // }
 
+class Time {
+    newDate = new Date()
+    hours = this.newDate.getHours()
+    minutes = this.newDate.getMinutes()
+    morning = true
+
+    getTimeOfDay() {
+        if (this.hours > 12) {
+            this.morning = false
+        }
+        return this.morning
+    }
+    getHours() {
+        if (this.hours > 12 && this.minutes > 30) {
+            this.hours = (this.hours - 12)
+        }
+        else {
+            this.hours
+        }
+        return this.hours
+    }
+    getMinutes() {
+        console.log(this.minutes)
+        return this.minutes
+    }
+
+}
+
 window.addEventListener("load", loadPage)
+
 function loadPage() {
     const rows: number = 11
     const cols: number = 11
-    let words: string[] = ['the', 'time', 'is']
-    let alphabets: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    drawTable(rows, cols)
+    drawText(cols)
+}
 
+function drawTable(rows: number, cols: number) {
+    const alphabets: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     const data = []
+
     for (let i = 0; i <= rows; i++) {
         const row = []
         for (let j = 0; j <= cols; j++) {
@@ -28,27 +61,97 @@ function loadPage() {
         rowData.forEach((cellData, j) => {
             const cell = row.insertCell(j)
             cell.innerText = cellData
-
         })
     })
-    console.log(table)
+}
 
+function drawText(cols: number) {
+    const words: string[] = ['the', 'time', 'is']
+    // const minutesText: string[] = ['five', 'ten','quarter', 'twenty', 'twenty-five','half']
+    // const minutesTo: string[] = [ 'past', 'to']
+    let time = new Time()
     const allRows = document.querySelectorAll('tr')
-    const eachCell = allRows[0].childNodes as any
-    const wordLength = words[0].length
-    console.log(wordLength)
 
-    console.log(eachCell[0])
+    drawRelevantArray(words, 0, cols, allRows)
+    drawTimeText(11, cols, allRows)
+    drawHours(time, 5, cols, allRows)
+    drawMinutes(time,8,cols,allRows)
+    convertNumberToName(time.getMinutes())
+    //drawMinutesText(minutesText, 4, cols, allRows)
+    // drawRelevantArray(words, 0, cols)
+    // drawRelevantArray(words, 0, cols)
+}
 
-    const startNumber = Math.floor(Math.random() * (cols - wordLength))
+function drawRelevantArray(array: string[], rowNumber: number, cols: number, allRows: NodeListOf<HTMLTableRowElement>) {
+    for (const item of array) {
+        const startNumber = Math.floor(Math.random() * (cols - item.length))
+        let eachCell = allRows[rowNumber].childNodes as NodeListOf<HTMLTableDataCellElement>
+        for (let i = 0; i < item.length; i++) {
+            let character = array[rowNumber].charAt(i)
+            eachCell[startNumber + i].innerText = character
+            eachCell[startNumber + i].classList.add("highlight")
+        }
+        rowNumber++
+    }
+}
 
-    console.log(words[0])
-    for (let i = 0; i < wordLength; i++) {
-        let character = words[0].charAt(i)
-        console.log(character)
+function drawTimeText(rowNumber, cols: number, allRows: NodeListOf<HTMLTableRowElement>) {
+    let time = new Time()
+    let timeOfDay
+    if (time.getTimeOfDay()) {
+        timeOfDay = 'AM'
+    }
+    else {
+        timeOfDay = 'PM'
+    }
+    let eachCell = allRows[rowNumber].childNodes as NodeListOf<HTMLTableDataCellElement>
+    placeRandomly(cols, timeOfDay, eachCell)
+}
+
+function drawHours(time, rowNumber, cols: number, allRows: NodeListOf<HTMLTableRowElement>) {
+    let hours = convertNumberToName(time.getHours())
+    let eachCell = allRows[rowNumber].childNodes as NodeListOf<HTMLTableDataCellElement>
+    placeRandomly(cols, hours, eachCell)
+}
+function drawMinutes(time, rowNumber, cols: number, allRows: NodeListOf<HTMLTableRowElement>) {
+    let minutes = convertNumberToName(time.getMinutes())
+    let eachCell = allRows[rowNumber].childNodes as NodeListOf<HTMLTableDataCellElement>
+    placeRandomly(cols, minutes, eachCell)
+}
+
+function convertNumberToName(num) {
+    const lowNames = ["zero", "one", "two", "three",
+        "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen",
+        "fifteen", "sixteen", "seventeen",
+        "eighteen", "nineteen"]
+    const tensNames = ["twenty", "thirty", "forty", "fifty",
+        "sixty", "seventy", "eighty", "ninety"]
+    let tens, ones, result
+    if (num < lowNames.length) {
+        result = lowNames[num]
+    } else {
+        tens = Math.floor(num / 10)
+        ones = num % 10
+        if (tens <= 9) {
+            result = tensNames[tens - 2]
+            if (ones > 0) {
+                result += "-" + lowNames[ones]
+            }
+        } else {
+            result = "notime"
+        }
+    }
+    console.log(result)
+    return result
+}
+
+
+function placeRandomly(cols, item, eachCell) {
+    const startNumber = Math.floor(Math.random() * (cols - item.length))
+    for (let i = 0; i < item.length; i++) {
+        let character = item.charAt(i)
         eachCell[startNumber + i].innerText = character
         eachCell[startNumber + i].classList.add("highlight")
     }
-
 }
-
